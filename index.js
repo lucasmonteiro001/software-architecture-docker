a0c280cb68881fdd671b79e2fca74c53350a473e
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-05-23 22:31:18 -0300

    Reading files

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-/**
- * Created by lucas on 5/23/17.
- */
-console.log("oi");
+const fs = require('fs');
+const YAML = require("yamljs");
+
+const dockerFilesFolder = './docker-compose';
+
+const readFileSync = (file) =>
+    new Promise((resolve, reject) => {
+        fs.readFile(file, 'utf8', function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
+    });
+
+const getFiles = new Promise((resolve) => {
+
+    let files = [];
+
+    fs.readdirSync(dockerFilesFolder).forEach(function (file) {
+        files.push(file);
+    });
+
+    resolve(files);
+});
+
+
+getFiles
+    .then(files => {
+
+        let yamlObjects = [];
+
+        files.map(file => {
+            readFileSync(`${dockerFilesFolder}/${file}`)
+                .then((fileContent) => {
+                    // console.log(YAML.parse(fileContent))
+                    yamlObjects.push(YAML.parse(fileContent));
+                    console.log(fileContent)
+                })
+                .catch(err => console.warn(err));
+        });
+
+    })
