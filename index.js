bb795d2b468e22ff322df236bbe77a338aa935cc
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-05-24 22:25:29 -0300

    Extracting metrics

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const getObjectLength = (object) => object ? Object.keys(object).length : 0;
 
 const getMetric = (service, metric) => service[metric] ? service[metric].length : 0;
 
+const getTotal = (obj) => {
+    let cont = 0;
+    for (let key in obj) {
+        cont += obj[key];
+    }
+    return cont;
+};
+
 const extractMetrics = (yamlObject) => {
 
     let metrics = {};
@@ -16,8 +24,9 @@ const extractMetrics = (yamlObject) => {
     let networks = yaml.networks;
 
     // Get metrics only for files with "services"
-    if(services) {
+    if (services) {
 
+        metrics.file = file;
         metrics.version = parseFloat(yaml.version);
         metrics.numberOfServices = getObjectLength(services);
         metrics.numberOfNetworks = getObjectLength(networks);
@@ -26,7 +35,7 @@ const extractMetrics = (yamlObject) => {
         metrics.volumes = [];
 
         // Extract metrics for each service
-        for(serviceKey in services) {
+        for (serviceKey in services) {
             let service = services[serviceKey];
             metrics.dependsOn.push(getMetric(service, 'depends_on'));
             metrics.ports.push(getMetric(service, 'ports'));
@@ -43,5 +52,96 @@ getAllFiles.then(files => {
     // Extract metrics for files
     let metricsPerFile = files.map(extractMetrics).filter(d => !!d);
 
-    console.log(metricsPerFile)
+    // Extract general metrics
+    let metrics = {};
+
+    metrics.version = {};
+    metrics.numberOfServices = {};
+    metrics.numberOfNetworks = {};
+    metrics.dependsOn = {};
+    metrics.ports = {};
+    metrics.volumes = {};
+
+    metricsPerFile.map(fileMetric => {
+
+        // console.log(fileMetric)
+
+        //////////////////////////////////ANALISE ARQUIVO//////////////////////////
+        /**
+         * Em relação ao arquivo
+         */
+        if (metrics.version[fileMetric.version]) {
+            metrics.version[fileMetric.version] += 1;
+        } else {
+            metrics.version[fileMetric.version] = 1;
+        }
+        /**
+         * Em relação ao arquivo
+         */
+        if (metrics.numberOfServices[fileMetric.numberOfServices]) {
+            metrics.numberOfServices[fileMetric.numberOfServices] += fileMetric.numberOfServices;
+        } else {
+            metrics.numberOfServices[fileMetric.numberOfServices] = fileMetric.numberOfServices;
+        }
+        /**
+         * Em relação ao arquivo
+         */
+        if (metrics.numberOfNetworks[fileMetric.numberOfNetworks]) {
+            metrics.numberOfNetworks[fileMetric.numberOfNetworks] += 1;
+        } else {
+            metrics.numberOfNetworks[fileMetric.numberOfNetworks] = 1;
+        }
+        //////////////////////////////////ANALISE ARQUIVO//////////////////////////
+
+
+        //////////////////////////////////ANALISE SERVICO//////////////////////////
+        /**
+         * Em relação ao serviço
+         */
+        fileMetric.dependsOn.map((dependsOn => {
+            if (metrics.dependsOn[dependsOn]) {
+                metrics.dependsOn[dependsOn] += 1;
+            } else {
+                metrics.dependsOn[dependsOn] = 1;
+            }
+        }));
+        /**
+         * Em relação ao serviço
+         */
+        fileMetric.ports.map((ports => {
+            if (metrics.ports[ports]) {
+                metrics.ports[ports] += 1;
+            } else {
+                metrics.ports[ports] = 1;
+            }
+        }));
+        /**
+         * Em relação ao serviço
+         */
+        fileMetric.volumes.map((volumes => {
+            if (metrics.volumes[volumes]) {
+                metrics.volumes[volumes] += 1;
+            } else {
+                metrics.volumes[volumes] = 1;
+            }
+        }));
+        //////////////////////////////////ANALISE SERVICO///////////////////////////
+    });
+
+    let finalMetrics = {
+        serv_dependsOn: metrics.dependsOn,
+        serv_dependsOnTotal: getTotal(metrics.dependsOn),
+        file_numberOfNetworks: metrics.numberOfNetworks,
+        file_numberOfNetworksTotal: getTotal(metrics.numberOfNetworks),
+        file_numberOfServices: metrics.numberOfServices,
+        file_numberOfServicesTotal: getTotal(metrics.numberOfServices),
+        serv_ports: metrics.ports,
+        serv_portsTotal: getTotal(metrics.ports),
+        file_version: metrics.version,
+        file_versionTotal: getTotal(metrics.version),
+        serv_volumes: metrics.volumes,
+        serv_volumesTotal: getTotal(metrics.volumes),
+    };
+
+    console.log(finalMetrics)
 });
